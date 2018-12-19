949bf40ceb492fe21f1775f0c6b2d0a82711f351
Captainmanthedan <Captainmanthedan@users.noreply.github.com>
2018-12-19 11:36:50 +0000

    Setup

---
+++ Prototypes/C#- winforms/Signup/Signup/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Signup
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			string username = UsernameBox.Text;
+
+			string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=test;";
+		}
+	}
+}
