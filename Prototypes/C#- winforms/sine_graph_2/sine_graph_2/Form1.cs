d69cb1e8a7c40e4321694157b24989561b7d651a
Captainmanthedan <Captainmanthedan@users.noreply.github.com>
2019-01-08 08:58:25 +0000

    Sine graph equation generates points

--- Prototypes/C#- winforms/sine_graph_2/sine_graph_2/Form1.cs
+++ Prototypes/C#- winforms/sine_graph_2/sine_graph_2/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace sine_graph_2
 {
@@ -29,27 +30,29 @@ namespace sine_graph_2
 			double y;
 			int x = 0;
 
-			double[,] points = new double[0,24];
+			double[] points = new double[25];
+
+			Tuple<int, int> t = Tuple.Create(0, 1);
 
 			while (0 <= x && x <= 360)
 			{
 				y = a * (Math.Sin((c * x) + d)) + b;
 
-				points[0,i] = y;
+				points[i] = y;
 
-				MessageBox.Show(Convert.ToString(x));
+				chart1.Series[0].Points.Add(new DataPoint(i, t.Item1));
+				t = Tuple.Create(t.Item2, t.Item1 + t.Item2);
 
 				x = x + 15;
 				i++;
 			}
 
 			i = 0;
+			x = 0;
 
 			while (0 <= x && x <= 360)
 			{
-				y = points[x, i];
-
-				textBox1.Text = textBox1.Text + "Point (" + x + "," + y + ")";
+				y = points[i];
 
 				x = x + 15;
 				i++;
