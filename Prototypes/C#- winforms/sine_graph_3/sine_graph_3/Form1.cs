407e3248c9b15249b041a1f66cd57a39ee5d6ad1
Captainmanthedan <36987095+Captainmanthedan@users.noreply.github.com>
2019-01-11 13:53:23 +0000

    Sine graph 3 comments and sine 2

--- Prototypes/C#- winforms/sine_graph_3/sine_graph_3/Form1.cs
+++ Prototypes/C#- winforms/sine_graph_3/sine_graph_3/Form1.cs
@@ -19,45 +19,38 @@ namespace sine_graph_3
 			chart1.Series[0].IsVisibleInLegend = false;
 		}
 
+		//When the button is pressed it uses a sine equation to generate corrdinates for an sine graph
 		private void button1_Click(object sender, EventArgs e)
 		{
-			int a = 1;
-			int b = 0;
-			int c = 1;
-			int d = 0;
-
-			int i = 0;
-
+			//this set thew starting x corrdinate to zero
 			int x_corrdinate = 0;
+
+			//this stores the y_corrdinate and is a double because
 			double y_corrdinate;
 
+			//this stores the x corrdinate in radians
 			double x_rad;
 
+			//this makes the chart be a Spline which is a line graph but each line is curved not straight.
 			chart1.Series[0].ChartType = SeriesChartType.Spline;
 
-			chart1.ChartAreas[0].AxisX.MajorGrid.LineWidth = 0;
-			chart1.ChartAreas[0].AxisY.MajorGrid.LineWidth = 0;
-
-			chart1.ChartAreas[0].AxisX.LineColor = chart1.BackColor;
-			chart1.ChartAreas[0].AxisY.LineColor = chart1.BackColor;
-
-			//removes number scale from the x and y axis of the graph
-			chart1.ChartAreas[0].AxisX.LabelStyle.Enabled = false;
-			chart1.ChartAreas[0].AxisY.LabelStyle.Enabled = false;
-
-			chart1.ChartAreas[0].AxisX.MajorTickMark.Enabled = false;
-			chart1.ChartAreas[0].AxisY.MajorTickMark.Enabled = false;
-
+			//this while loop iterates through from x = 0 to x = 1440, adding 90 to x on each iteration
 			while (0 <= x_corrdinate && x_corrdinate <= 1440)
 			{
+				//this connverts the x corrdinate from degrees to radians so that it can be used to calculate the y corrdinate
 				x_rad = x_corrdinate * (Math.PI / 180);
 
-				y_corrdinate = a * (Math.Sin((c * x_rad) + d)) + b;
+				//this sine equation generates the y corrdinate by using sine on the current x corrdinate in radians
+				y_corrdinate = Math.Sin(x_rad);
 
+				//this rouns the y corrdinate to a whole number
+				y_corrdinate = Math.Round(y_corrdinate);
+
+				//this adds a data point on the graph at the x corrdinate (in degrees) and the y corrdinate
 				chart1.Series[0].Points.Add(new DataPoint(x_corrdinate, y_corrdinate));
 
+				//this adds 90 degrees to the x corrdinate
 				x_corrdinate = x_corrdinate + 90;
-				i++;
 			}
 		}
 	}
--- Prototypes/C#- winforms/sine_graph_4/sine_graph_4/Form1.cs
+++ Prototypes/C#- winforms/sine_graph_4/sine_graph_4/Form1.cs
@@ -7,19 +7,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
-namespace sine_graph_2
+namespace sine_graph_4
 {
 	public partial class Form1 : Form
 	{
 		public Form1()
 		{
 			InitializeComponent();
+			chart1.Series[0].IsVisibleInLegend = false;
 		}
 
 		//When the button is pressed it uses a sine equation to generate corrdinates for an sine graph
 		private void button1_Click(object sender, EventArgs e)
 		{
+			int a = 1;
+			int b = 0;
+			int c = 1;
+			int d = 0;
+
 			//this set thew starting x corrdinate to zero
 			int x_corrdinate = 0;
 
@@ -29,25 +36,36 @@ namespace sine_graph_2
 			//this stores the x corrdinate in radians
 			double x_rad;
 
-			//this sets the text box to NULL so that I can add to it later
-			textBox1.Text = "";
+			//this makes the chart be a Spline which is a line graph but each line is curved not straight.
+			chart1.Series[0].ChartType = SeriesChartType.Spline;
+
+			chart1.ChartAreas[0].AxisX.MajorGrid.LineWidth = 0;
+			chart1.ChartAreas[0].AxisY.MajorGrid.LineWidth = 0;
+
+			chart1.ChartAreas[0].AxisX.LineColor = chart1.BackColor;
+			chart1.ChartAreas[0].AxisY.LineColor = chart1.BackColor;
+
+			//removes number scale from the x and y axis of the graph
+			chart1.ChartAreas[0].AxisX.LabelStyle.Enabled = false;
+			chart1.ChartAreas[0].AxisY.LabelStyle.Enabled = false;
+
+			chart1.ChartAreas[0].AxisX.MajorTickMark.Enabled = false;
+			chart1.ChartAreas[0].AxisY.MajorTickMark.Enabled = false;
 
 			//this while loop iterates through from x = 0 to x = 1440, adding 90 to x on each iteration
 			while (0 <= x_corrdinate && x_corrdinate <= 1440)
 			{
 				//this connverts the x corrdinate from degrees to radians so that it can be used to calculate the y corrdinate
-				x_rad = x_corrdinate * (Math.PI / 180);
+				x_rad = (x_corrdinate) * (Math.PI / 180);
 
 				//this sine equation generates the y corrdinate by using sine on the current x corrdinate in radians
-				y_corrdinate = Math.Sin(x_rad);
+				y_corrdinate = a * (Math.Sin((c * x_rad) + d)) + b;
 
 				//this rouns the y corrdinate to a whole number
 				y_corrdinate = Math.Round(y_corrdinate);
 
-				//this adds the next x corrdinate (in degrees) and y corrdinate to the text box
-				textBox1.Text = textBox1.Text + "(" + x_corrdinate + ", " + y_corrdinate + ")";
-				//this adds a new line to the text box
-				textBox1.AppendText(Environment.NewLine);
+				//this adds a data point on the graph at the x corrdinate (in degrees) and the y corrdinate
+				chart1.Series[0].Points.Add(new DataPoint(x_corrdinate, y_corrdinate));
 
 				//this adds 90 degrees to the x corrdinate
 				x_corrdinate = x_corrdinate + 90;
